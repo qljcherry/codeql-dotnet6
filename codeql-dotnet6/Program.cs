7cc42366ec064a0b972fe3d82fd63a055e19c2aa
JCherry <joshcherry@quickenloans.com>
2021-11-11 16:33:23 -0500

    initial commit

---
+++ codeql-dotnet6/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Net.Http;
+
+var client = new HttpClient();
+var response = client.GetAsync("https://google.com");
+Console.WriteLine($"Response code: {response.Status}");
